f78af95347303fecdb12f8f3649ff477642ed27c
Vinicìus Cezário <vinicezario@hotmail.com>
2025-06-04 09:54:44 -0300

    Creat app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+velocidade = int(input("Digite sua velocidade"))
+velocidade_maxima = 80
+if velocidade <= velocidade_maxima:
+    print("Não Levou Muta")
+elif velocidade > velocidade_maxima and velocidade <= velocidade_maxima + 10;
+    print("Levou multa")
+elif velocidade > velocidade_maxima + 11 and velocidade <= velocidade_maxima + 20:
+    print("Levou multa grave")
+      
